98fc7368a41e450935a2a23e365af2631ca515ea
stoneliuCS <liu.sto@northeastern.edu>
2023-12-01 19:41:52 -0500

    clip backend running

--- main.py
+++ main.py
@@ -1,7 +1,80 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from transformers import AutoProcessor, AutoModelForZeroShotImageClassification
+import base64
+from io import BytesIO
+from PIL import Image
 
-if __name__ == "__main__":
-    import uvicorn
+#Allowing every front-end application 
+origins = ["*"]
+app = FastAPI()
 
-    # Run the FastAPI application
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods= ["*"],
+    allow_headers= ["*"]
+)
+
+class ImageData(BaseModel):
+    image: str
+
+@app.post("/image2text")
+def image2text(body: ImageData):
+    base64_string = body.image 
+    image_data = base64.b64decode(base64_string)
+    image = Image.open(BytesIO(image_data))
+
+    processor = AutoProcessor.from_pretrained("openai/clip-vit-large-patch14") 
+
+    model = AutoModelForZeroShotImageClassification.from_pretrained("openai/clip-vit-large-patch14")
+
+
+
+    features = ["Jacket", "T-Shirt", "Shoe", "Pants", "Hats", "Glasses", "Dress"]
+
+    colors = ["Black", "Red", "Blue", "Yellow", "Green", "White", "Brown", "Gray"]
+
+    inputs = processor(
+        text=features,
+        images=image,
+        return_tensors="pt",
+        padding=True,
+    )
+
+    color_inputs = processor(
+        text=colors,
+        images=image,
+        return_tensors="pt",
+        padding=True,
+    )
+
+    outputs = model(**inputs)
+    logits_per_image = outputs.logits_per_image
+    probs = logits_per_image.softmax(dim=1)
+
+    color_outputs = model(**color_inputs)
+    color_logits_per_image = color_outputs.logits_per_image
+    color_probs = color_logits_per_image.softmax(dim=1)
+
+    prob_list = probs.detach().numpy().tolist()
+
+    color_prob_list = color_probs.detach().numpy().tolist()
+
+    output = dict(zip(features, prob_list[0]))
+
+    print(output)
+
+    clothing_item = max(output, key=output.get)
+
+    color_output = dict(zip(colors, color_prob_list[0]))
+
+    print(color_output)
+
+    color = max(color_output, key=color_output.get)
+
+    return color + " " + clothing_item
+
+
